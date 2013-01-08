7f3d77a017084ef8cf139588835b68982e62eb09
yangxd <yangxd@ferrotec.com.cn>
2013-01-08 20:31:12 +0800

    用CSS重构首页 老的界面用了不少table以及针对IE6写了太多的代码，对后来的修改很不利，所以有时间就修改过来。 Signed-off-by: yangxd <yangxd@ferrotec.com.cn>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ftweb::Application.config.secret_token = 'dbea434c9f31a44b645a37fbb3a0d6d49014a711f95360262a91eb0f405241730e2cbae3b35b63b9ea4747be84c4c5499bc939558f87f856f9ad43094c591301'
